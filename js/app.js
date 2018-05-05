0aeef6176f969a748c6a548802150e396955e46f
zach-col <theperfectpicture100@gmail.com>
2018-05-05 02:44:12 -0500

    feat: add memory match features

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -24,6 +12,89 @@ function shuffle(array) {
 
     return array;
 }
+
+/*  Create a list that holds all of your cards */
+let cardsList = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-cube','fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-bolt', 'fa-diamond','fa-paper-plane-o','fa-anchor','fa-cube','fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-bolt'];
+
+/* shuffle cards */
+shuffle(cardsList)
+
+/* get card elements on page */
+let cardsElements = document.getElementsByClassName("customFa");
+
+/* get cards */
+let cards = document.getElementsByClassName("card");
+
+/* add card faces to elements */
+for (let i = 0; i < cardsElements.length; i++) {
+      cardsElements[i].className += cardsList[i] ;
+}
+
+for(var i=0; i< cards.length; i++) {
+  cards[i].addEventListener("click", toggleClass(i));
+}
+
+let recentlyClicked = [];
+
+// change elements to hidden
+function changeElementsToHidden(){
+    setTimeout(function(){
+      console.log("hji")
+    }, 2000);
+     // remove open show classes
+     recentlyClicked[0].classList.remove("open", "show");
+     recentlyClicked[1].classList.remove("open", "show");
+
+}
+
+// change recently clicked elements to matched
+function changeElementsToMatched(){
+     changeElementsToHidden()
+     // add matched
+     recentlyClicked[0].classList.add("match");
+     recentlyClicked[1].classList.add("match");
+}
+
+function toggleClass(i) {
+  return function(){
+    /* check if card is a match*/
+   if(cards[i].classList.contains('match')){
+    return;
+   }
+   /* check if item is already shown */
+   else if(cards[i].classList.contains('open' || 'show')){
+    return;
+   }
+
+   // add class show and open to show card element
+   cards[i].classList.add("open","show");
+
+   // add card element to recentyClicked array
+   recentlyClicked.unshift(cards[i])
+
+   // check if 2 cards have been clicked
+   if(recentlyClicked.length == 2){
+     // compare if recently clicked cards are a match
+     if(recentlyClicked[0].isEqualNode(recentlyClicked[1])){
+       // change recently clicked to matched styling
+       changeElementsToMatched();
+       // remove clicked elements
+       recentlyClicked.splice(0, 2);
+       return;
+     }
+       // hide elements
+       changeElementsToHidden();
+     // remove recently clicked items from array
+     recentlyClicked.splice(0, 2);
+   }
+
+  }
+}
+
+
+
+
+
 
 
 /*
